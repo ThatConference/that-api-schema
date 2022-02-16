dfff7b60cbfed18494b10a51750d56e894fcd595
Brett Slaski <brett@thatconference.com>
2022-02-16 09:39:02 -0600

    Add LinkType enum. Package bumps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import productTypeEnumDoc from './typeDefs/enums/productType';
 import bufferSocialProfile from './typeDefs/enums/bufferSocialProfile';
+import linkType from './typeDefs/enums/linkType';
 import resolveTypeImpl from './resolveType';
 
 export const productTypeEnum = productTypeEnumDoc;
 export const bufferSocialProfileEnum = bufferSocialProfile;
+export const linkTypeEnum = linkType;
 export const resolveType = resolveTypeImpl;

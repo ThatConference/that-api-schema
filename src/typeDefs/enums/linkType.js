06503da16f55437a699c773d1bfb12041317aa03
Brett Slaski <brett@thatconference.com>
2022-11-10 09:00:49 -0600

    Add new link types

--- src/typeDefs/enums/linkType.js
+++ src/typeDefs/enums/linkType.js
@@ -2,36 +2,50 @@ import { mergeTypeDefs } from '@graphql-tools/merge';
 
 const linkTypeEnum = `
   enum LinkType {
-    "LinkedIn"
-    LINKEDIN
-    "GitHub"
-    GITHUB
-    "You Tube"
-    YOUTUBE
-    "Instagram"
-    INSTAGRAM
-    "Twitter"
-    TWITTER
-    "Facebook"
-    FACEBOOK
-    "Twitch"
-    TWITCH
-    "TIKTOK"
-    TIKTOK
-    "Website"
-    WEBSITE
+    "BeReal."
+    BEREAL
+    "Blog"
+    BLOG
     "DEV To"
     DEV_TO
     "Dribbble"
     DRIBBBLE
+    "Facebook"
+    FACEBOOK
+    "GitHub"
+    GITHUB
+    "Instagram"
+    INSTAGRAM
+    "LinkedIn"
+    LINKEDIN
+    "Mastodon"
+    MASTODON
     "Medium"
     MEDIUM
+    "Reddit"
+    REDDIT
+    "Snapchat"
+    SNAPCHAT
     "Stack Overflow"
     STACK_OVERFLOW
-    "Blog"
-    BLOG
+    "Telegram"
+    TELEGRAM
+    "TIKTOK"
+    TIKTOK
+    "Twitch"
+    TWITCH
+    "Twitter"
+    TWITTER
+    "VERO"
+    VERO
     "Vlog"
     VLOG
+    "WhatsApp"
+    WHATSAPP
+    "Website"
+    WEBSITE
+    "You Tube"
+    YOUTUBE
   }
 `;
 
